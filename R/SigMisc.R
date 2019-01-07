b39e2bde44d9782a8af58526b393d42e147f6564
Steven G. Rozen <steve@steverozen.net>
2019-01-08 00:24:44 +0800

    updated documentation again

--- R/SigMisc.R
+++ R/SigMisc.R
@@ -9,7 +9,8 @@
 #' \code{\link{Match1Sig}},
 #' \code{\link{MatchSigs1Direction}},
 #' \code{\link{MatchSigs2Directions}},
-#' \code{\link{MatchSigsThenWriteAndPlot}}
+#' \code{\link{MatchSigsThenWriteAndPlot}},
+#' \code{\link{TestMatchSigs2Directions}}
 #'
 #' @section Miscellaneous:
 #' fwriteDataFrame
