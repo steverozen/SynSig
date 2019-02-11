451b8f7a652a5b89adcf831b6d2664da5b0f3e34
steverozen <steve@steverozen.net>
2019-02-11 18:47:20 +0800

    more tweaks

--- data-raw/TestRunSA.R
+++ data-raw/TestRunSA.R
@@ -13,7 +13,7 @@ wrapper <- function() {
   # as specified input and output locations
   # realtive to here.
 
-  for (i in 1:2) {
+  for (i in 1:5) {
       RunSignatureAnalyzerOnFile(
         # "syn_sbs3_5_40/sp.sp.RCC.and.OVA.syn.data.csv",
         "../vignettes/syn_3_5_40_abst_v2/sp.sp.abst.syn.data.csv",
@@ -21,6 +21,8 @@ wrapper <- function() {
         paste0("../../saX.test.abst.2019.syn_3_5_40_abst_v2.02.10.work", i),
         # paste0("../../sa.test.2019.02.10.home", i),
         WriteCat96,
+        input.exposures =
+          "../vignettes/syn_3_5_40_abst_v2/sp-3-5-40-abst-syn-exp.csv",
         test.only = FALSE)
   }
 
