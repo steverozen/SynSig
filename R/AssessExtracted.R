d8e8ba9c1e467515f4abf41ca3b9f348380643d7
steverozen <steve@steverozen.net>
2019-02-19 11:45:04 +0800

    corrected spelling

--- R/AssessExtracted.R
+++ R/AssessExtracted.R
@@ -77,36 +77,6 @@ MatchSigsAndRelabel <-
     invisible(sim)
   }
 
-#' @title Write exposure matrix to a file
-#'
-#' @param exposure.matrix Matrix of exposures
-#'
-#' @param file File to which to write the exposure matrix (as a CSV file)
-#'
-#' @export
-#'
-#' @importFrom utils write.csv
-#'
-WriteExposure <- function(exposure.matrix, file) {
-  old.digits <- getOption("digits")
-  options(digits = 22)
-  write.csv(exposure.matrix, file, row.names = TRUE)
-  options(digits = old.digits)
-}
-
-#' @title Read an exposure matrix to a file
-#'
-#' @param file CSV file containing an exposure matrix
-#'
-#' @return Matrix of exposures
-#'
-#' @export
-#'
-#' @importFrom utils read.csv
-ReadExposure <- function(file) {
-  return(read.csv(file, row.names = 1))
-}
-
 #' @title Assess how well extracted signatures match input signatures
 #'
 #' We assume that in many cases extraction programs will be run
