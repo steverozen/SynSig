502bd4c428844fb45be8f230e530e8dfafde81f7
steverozen <steve@steverozen.net>
2019-02-19 14:29:44 +0800

    added model script to run SA on monster server

---
+++ data-raw/run.signatureanalyzer.on.monster.R
@@ -0,0 +1,15 @@
+# Put this file in the top level directory and start R from there.
+
+library(ICAMS)
+library(SynSig)
+
+set.seed(888)
+INPUT <<- "INPUT_SignatureAnalyzer/"
+test.abst <- SignatureAnalyzer4MatchedCatalogs(
+  num.runs = 20,
+  signatureanalyzer.code.dir = "/home/gmssgr/bin/SignatureAnalzyer.052418/",
+  dir.root = ".",
+  slice = 1,
+  overwrite = FALSE,
+  mc.cores = 10)
+
