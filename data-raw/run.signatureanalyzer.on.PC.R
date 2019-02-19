8f0128fac2525df5a084976cc9b680d3892e550b
steverozen <steve@steverozen.net>
2019-02-19 14:31:07 +0800

    updated script to run SA on Windows

--- data-raw/run.signatureanalyzer.on.PC.R
+++ data-raw/run.signatureanalyzer.on.PC.R
@@ -5,6 +5,10 @@ set.seed(888)
 INPUT <<- "INPUT_SignatureAnalyzer/"
 test.abst <- SignatureAnalyzer4MatchedCatalogs(
   num.runs = 3,
-  signatureanalyzer.code.dir = "./SignatureAnalzyer.052418/",
-  dir.root = ".", slice = 1)
+  signatureanalyzer.code.dir =
+    "C:/Users/steve/Documents/SynSig/data-raw/SignatureAnalzyer.052418/",
+  dir.root = "c:/users/steve/Documents/0syn.3.5.40.abst.v8/",
+  slice = 1,
+  overwrite = TRUE,
+  mc.cores = 1)
 
