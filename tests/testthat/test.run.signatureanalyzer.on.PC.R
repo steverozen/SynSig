bafb529599e9e0f289d64fa9da4dfdd446c5b592
steverozen <steve@steverozen.net>
2019-03-06 10:43:41 +0800

    added regression data

--- tests/testthat/test.run.signatureanalyzer.on.PC.R
+++ tests/testthat/test.run.signatureanalyzer.on.PC.R
@@ -1,11 +1,22 @@
+context("Long test of SignatureAnalyzer4MatchedCatalogs")
 
-set.seed(888)
-test.abst <- SignatureAnalyzer4MatchedCatalogs(
-  num.runs = 2,
-  signatureanalyzer.code.dir =
-    "../../data-raw/SignatureAnalzyer.052418/",
-  dir.root = "test.random.sigs/",
-  slice = 1,
-  test.only = TRUE, # Just use a small subset of the input catalog
-  overwrite = TRUE,
-  mc.cores = 1)
+test_that("SignatureAnalyzer4MatchedCatalogs", {
+  # load("tmp.test.Rdata")
+  load("SignatureAnalyzer4MatchedCatalogs.out.Rdata")
+  set.seed(888)
+  tmp.result <<-
+    SignatureAnalyzer4MatchedCatalogs(
+      num.runs = 2,
+      signatureanalyzer.code.dir =
+        "../../data-raw/SignatureAnalzyer.052418/",
+      dir.root = "test.random.sigs/",
+      slice = 1,
+      test.only = TRUE, # Just use a small subset of the input catalog
+      overwrite = TRUE,
+      mc.cores = 1)
+  # save(SignatureAnalyzer4MatchedCatalogs.out,
+  #     file = "SignatureAnalyzer4MatchedCatalogs.out.Rdata")
+  expect_equal(
+    tmp.result,
+    SignatureAnalyzer4MatchedCatalogs.out)
+})
