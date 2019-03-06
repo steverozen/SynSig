e135c2873b346da03388045b7c3c19c69b48f30c
steverozen <steve@steverozen.net>
2019-03-06 09:01:18 +0800

    update tests in test.summarize.one.dir.R

--- tests/testthat/test.summarize.one.dir.R
+++ tests/testthat/test.summarize.one.dir.R
@@ -1,40 +1,60 @@
 context("Test summarizing extraction results in one directory")
 
-# TODO (Steve): clean up
-singature.analyzer.sa.sa.COMPOSITE.out <-
-  SummarizeSigOneSACOMPOSITESubdir(
-    third.level.dir = "./test.sa.sa.COMPOSITE/sa.results/",
-    ground.truth.exposure.name = "ground.truth.syn.exposures.csv",
-    which.run = "1.run.sa.sa.COMPOSITE")
+test_that("signature.analyzer.sa.sa.COMPOSITE.out", {
+  load("signature.analyzer.sa.sa.COMPOSITE.out")
+  expect_equal(
+    SummarizeSigOneSACOMPOSITESubdir(
+      third.level.dir = "./test.sa.sa.COMPOSITE/sa.results/",
+      ground.truth.exposure.name = "ground.truth.syn.exposures.csv",
+      which.run = "1.run.sa.sa.COMPOSITE",
+      overwrite = FALSE),
+    signature.analyzer.sa.sa.COMPOSITE.out)
+  if (TRUE) # Set this to FALSE to inspect the file and directory output
+  {
+    # Clean up
+    unlink("./test.sa.sa.COMPOSITE/sa.results/summary",
+           recursive = TRUE, force = TRUE)
+  }
+})
 
-singature.analyzer.sa.sa.96.out <-
-  SummarizeSigOneSA96Subdir(
-    third.level.dir = "./test.sa.sa.96/sa.results/",
-    ground.truth.exposure.name = "ground.truth.syn.exposures.csv",
-    which.run = "1.run.sa.sa.96",
-    write.png = FALSE) # Set to false for some operating systems
-# TODO (Steve): clean up
+test_that("SummarizeSigOneSA96Subdir", {
+  load("signature.analyzer.sa.sa.96.out.Rdata")
+  expect_equal(
 
-test_that("SummarizeSigOneSPSubdir",
-          {
-            load("./test.sigprofiler.sp.sp.out.Rdata")
-            expect_equal(
-              SummarizeSigOneSPSubdir(
-                third.level.dir =
-                  "./sp.sum.test.in/sp.results/",
-                ground.truth.exposure.name = "ground.truth.syn.exposures.csv",
-                write.png = FALSE), # Set this to FALSE to speed up the test.
-              sigprofiler.sp.sp.out)
-            if (TRUE) # Set this to FALSE to inspect the file and directory output
-            {
-              # Clean up
-              unlink("./sp.sum.test.in/sp.results/summary",
-                     recursive = TRUE, force = TRUE)
-              unlink(
-                paste0("./sp.sum.test.in/",
-                       "sp.results/SBS96/Selected_Solution/",
-                       "De_Novo_Solution/signatures.PCAWG.format.csv"))
-            }
-          })
+    SummarizeSigOneSA96Subdir(
+      third.level.dir = "./test.sa.sa.96/sa.results/",
+      ground.truth.exposure.name = "ground.truth.syn.exposures.csv",
+      which.run = "1.run.sa.sa.96",
+      write.png = FALSE,  # Set to FALSE for some operating systems
+      overwrite = FALSE),
+    signature.analyzer.sa.sa.96.out)
+  if (TRUE) # Set this to FALSE to inspect the file and directory output
+  {
+    # Clean up
+    unlink("./test.sa.sa.96/sa.results/summary",
+           recursive = TRUE, force = TRUE)
+  }
+})
+
+test_that("SummarizeSigOneSPSubdir", {
+  load("./test.sigprofiler.sp.sp.out.Rdata")
+  expect_equal(
+    SummarizeSigOneSPSubdir(
+      third.level.dir =
+        "./sp.sum.test.in/sp.results/",
+      ground.truth.exposure.name = "ground.truth.syn.exposures.csv",
+      write.png = FALSE), # Set this to FALSE to speed up the test.
+    sigprofiler.sp.sp.out)
+  if (TRUE) # Set this to FALSE to inspect the file and directory output
+  {
+    # Clean up
+    unlink("./sp.sum.test.in/sp.results/summary",
+           recursive = TRUE, force = TRUE)
+    unlink(
+      paste0("./sp.sum.test.in/",
+             "sp.results/SBS96/Selected_Solution/",
+             "De_Novo_Solution/signatures.PCAWG.format.csv"))
+  }
+})
 
 
