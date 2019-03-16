62d0b2ecfc364cab2d668f92dcd09218b39c2e4d
steverozen <steve@steverozen.net>
2019-03-16 19:43:04 +0800

    add some checking test cleanup

--- tests/testthat/test.summarize.one.dir.R
+++ tests/testthat/test.summarize.one.dir.R
@@ -26,13 +26,17 @@ test_that("SummarizeSigOneSA96Subdir", {
       ground.truth.exposure.name = "ground.truth.syn.exposures.csv",
       which.run = "1.run.sa.sa.96",
       write.png = FALSE,  # Set to FALSE for some operating systems
-      overwrite = FALSE),
+      overwrite = TRUE),
     signature.analyzer.sa.sa.96.out)
   if (TRUE) # Set this to FALSE to inspect the file and directory output
   {
     # Clean up
-    unlink("./test.sa.sa.96/sa.results/summary",
+    file.to.unlink <- "./test.sa.sa.96/sa.results/summary"
+    if (!file.exists(file.to.unlink)) cat("Coding error,", file.to.unlink,
+                                          "does not exist\n")
+    res <- unlink(file.to.unlink,
            recursive = TRUE, force = TRUE)
+    if (res != 0) cat("Failed to unlink", file.to.unlink, "\n")
   }
 })
 
@@ -41,7 +45,7 @@ test_that("SummarizeSigOneSPSubdir", {
   expect_equal(
     SummarizeSigOneSPSubdir(
       third.level.dir =
-        "./sp.sum.test.in/sp.results/",
+        "./sp.sum.test.in/sp.results/", overwrite = T,
       ground.truth.exposure.name = "ground.truth.syn.exposures.csv",
       write.png = FALSE), # Set this to FALSE to speed up the test.
     sigprofiler.sp.sp.out)
@@ -51,12 +55,12 @@ test_that("SummarizeSigOneSPSubdir", {
     file1 <- "./sp.sum.test.in/sp.results/summary"
     if (!file.exists(file1)) cat("coding error, wrong file\n")
     res <- unlink(file1, recursive = TRUE, force = TRUE)
-    if (!res) cat("Failed to unlink", file1, "\n")
+    if (res != 0) cat("Failed to unlink", file1, "\n")
     res <- unlink(
       paste0("./sp.sum.test.in/",
              "sp.results/SBS96/Selected_Solution/",
              "De_Novo_Solution/signatures.PCAWG.format.csv"))
-    if (!res) cat("Failed to unlink several files\n")
+    if (res != 0) cat("Failed to unlink several files\n")
   }
 })
 
