d777553f417a39a445529a1eeaef85ae40fc7fd2
WuyangFF95 <1015497577@qq.com>
2019-04-27 13:55:01 +0800

    Temporarily disable MultiModalMuSigInteraction to prevent complaining about errors related to "NULL".

--- R/MultiModalMuSigInteraction.R
+++ R/MultiModalMuSigInteraction.R
@@ -2,36 +2,36 @@
 # Interacting functions for running MultiModalMuSig julia package
 
 # Convert SNS96 Catalogs from ICAMS format to MM format
-ICAMSSNS96Catalog2MM <- function(NULL) {
+#ICAMSSNS96Catalog2MM <- function(NULL) {
 
-  NULL
-}
+#  NULL
+#}
 
 # Convert SNS1536 Catalogs from ICAMS format to MM format
-ICAMSSNS1536Catalog2MM <- function(NULL) {
+#ICAMSSNS1536Catalog2MM <- function(NULL) {
 
-  NULL
-}
+#  NULL
+#}
 
 # Convert DNS78 Catalogs from ICAMS format to MM format
-ICAMSDNS96Catalog2MM <- function(NULL) {
+#ICAMSDNS96Catalog2MM <- function(NULL) {
 
-  NULL
-}
+#  NULL
+#}
 
 # Convert ID83 Catalogs from ICAMS format to MM format
-ICAMSID83Catalog2MM <- function(NULL) {
+#ICAMSID83Catalog2MM <- function(NULL) {
 
-  NULL
-}
+#  NULL
+#}
 
 # Convert COMPOSITE (SNS1536+DBS78+ID83) catalogues
 # in ICAMS format to MM format
-ICAMSCompositeCatalog2MM <- function(NULL) {
+#ICAMSCompositeCatalog2MM <- function(NULL) {
 
   # Split COMPOSITE catalog to 3 catalogues
   # (1 SNS, 1 DNS, 1 Indel)
   # use function in ReadWriteCatalogs.R
 
-  NULL
-}
+#  NULL
+#}
