8eb9500c224183201b62eca3af38690320655b70
WuyangFF95 <1015497577@qq.com>
2019-04-28 17:30:59 +0800

    V0.2.1.9004: Minor version change: will incorporate MultiModalMuSigInteraction.R and codes running other software packages.

--- R/MultiModalMuSigInteraction.R
+++ R/MultiModalMuSigInteraction.R
@@ -1,37 +1,152 @@
 # MultiModalMuSigInteraction.R
 # Interacting functions for running MultiModalMuSig julia package
 
-# Convert SNS96 Catalogs from ICAMS format to MM format
-#ICAMSSNS96Catalog2MM <- function(NULL) {
+#' Convert Catalogs from ICAMS format to MM format
+#'
+#' @param catalog A catalog matrix in ICAMS format. (SNS/DNS/ID)
+#'
+#' @return a catalog matrix in MultiModalMuSig format.
+ICAMSCatalog2MM <- function(catalog) {
+  # Read catalog. From matrix-like
+  stopifnot(is.data.frame(catalog) | is.matrix(catalog))
 
-#  NULL
-#}
+  catalog <- data.frame("term" = rownames(catalog),
+                          catalog)
+  return(catalog)
+}
 
-# Convert SNS1536 Catalogs from ICAMS format to MM format
-#ICAMSSNS1536Catalog2MM <- function(NULL) {
 
-#  NULL
-#}
 
-# Convert DNS78 Catalogs from ICAMS format to MM format
-#ICAMSDNS96Catalog2MM <- function(NULL) {
+#' Read Catalog files in MM format
+#' @param cat Input catalog, can be a tab-delimited text
+#' file in MultiModalMuSig format.
+#' @return a catalog matrix in ICAMS format.
+ReadCatMM <- function(cat){
 
-#  NULL
-#}
+ catMatrix <- read.table(file = cat, header = T,
+                         sep = "\t", as.is = T)
+ rownames(catMatrix) <- catMatrix[,1]
+ catMatrix <- catMatrix[,-1]
 
-# Convert ID83 Catalogs from ICAMS format to MM format
-#ICAMSID83Catalog2MM <- function(NULL) {
+ return(catMatrix)
+}
 
-#  NULL
-#}
 
-# Convert COMPOSITE (SNS1536+DBS78+ID83) catalogues
-# in ICAMS format to MM format
-#ICAMSCompositeCatalog2MM <- function(NULL) {
 
-  # Split COMPOSITE catalog to 3 catalogues
-  # (1 SNS, 1 DNS, 1 Indel)
-  # use function in ReadWriteCatalogs.R
+#' Convert Catalogs from MM format to ICAMS format
+#'
+#' @param cat Input catalog, can be a tab-delimited file
+#' or matrix in MultiModalMuSig format.
+#'
+#' @return a catalog matrix in ICAMS format.
 
-#  NULL
-#}
+MMCatalog2ICAMS <- function(cat) {
+
+  # Read MM-formatted catalog. Either from file or matrix-like
+  stopifnot(is.character(cat) | is.data.frame(cat) | is.matrix(cat))
+  if(class(cat) == "character") {
+    catMatrix <- ReadCatMM(cat)
+  } else {
+    catMatrix <- cat
+    rownames(catMatrix) <- cat[,1]
+    catMatrix <- catMatrix[,-1]
+  }
+  # For some SNS96 catalogs,
+  # rownames of catMatrix may be in MM format
+  # (A[C->A]G) rather than in ICAMS format (ACGA).
+  # In this case, the format will be changed.
+  if(nrow(catMatrix)==96 & grepl("->",rownames(catMatrix)[1])){
+
+    oldrowNames <- rownames(catMatrix)
+    newrowNames <- character(length(oldrowNames))
+    before <- character(length(oldrowNames))
+    ref <- character(length(oldrowNames))
+    after <- character(length(oldrowNames))
+    var <- character(length(oldrowNames))
+
+    for(ii in 1:length(oldrowNames)){
+      before[ii] <- strsplit(oldrowNames[ii],"\\[")[[1]][1]
+
+      ref[ii] <- strsplit(oldrowNames[ii],"->")[[1]][1]
+      ref[ii] <- strsplit(ref[ii],"\\[")[[1]][2]
+
+      after[ii] <- strsplit(oldrowNames[ii],"\\]")[[1]][2]
+
+      var[ii] <- strsplit(oldrowNames[ii],"->")[[1]][2]
+      var[ii] <- strsplit(ref[ii],"\\]")[[1]][1]
+
+      newrowNames[ii] <- paste0(before[ii],ref[ii],after[ii],var[ii])
+    }
+
+    rownames(catMatrix) <- newrowNames
+  }
+
+
+  return(catMatrix)
+}
+
+#' Prepare input file for MultiModalMuSig from a
+#' MultiModalMuSig formatted catalog file.
+#'
+#' @param catalog a catalog in ICAMS format. It can be
+#' a .csv file, or a matrix or data.frame.
+#' Usually, it refers to \code{"ground.truth.syn.catalog.csv"}.
+#'
+#' @param read.catalog.function Function taking a file path as
+#' its only argument and returning a catalog as a numeric matrix.
+#'
+#' @param out.dir Directory that will be created for the output;
+#' abort if it already exits.  Log files will be in
+#' \code{paste0(out.dir, "/tmp")}.
+#'
+#' @param overwrite If TRUE, overwrite existing output
+#'
+#' @return \code{invisible(catMatrix)},
+#' original catalog in MultiModalMuSig format
+#'
+#' @details Creates folder named \code{MultiModalMuSig.results} containing catalogs
+#' in MultiModalMuSig-formatted catalogs: Rows are signatures;
+#' the first column is the name of the mutation type, while the remaining
+#' columns are samples (tumors).
+#' These MM-formated catalogs will the input when running MultiModalMuSig program
+#' later on Julia platform.
+#'
+#' @export
+#'
+#' @importFrom utils capture.output
+
+CreateMultiModalMuSigOutput <-
+  function(catalog = "ground.truth.syn.catalog.csv",
+           read.catalog.function = NULL,
+           out.dir = "MultiModalMuSig.results",
+           overwrite = FALSE) {
+
+  ## If catalog is a string of file path
+  if(is.character(catalog)){
+    ## Read in catalog matrix using read.catalog.function.
+    catMatrix <- read.catalog.function(catalog, strict = FALSE)
+    ## Convert catalog to MM format
+    catMatrix <- ICAMSCatalog2MM(catalog)
+    ## Fetch the name of catalog file without extension
+    oldFileName <- tools::file_path_sans_ext(basename(catalog))
+  }
+  if(is.data.frame(catalog) | is.matrix(catalog)){
+    ## Convert catalog to MM format
+    catMatrix <- ICAMSCatalog2MM(catalog)
+    ## Fetch the name of catalog file
+    oldFileName <- "catalog"
+  }
+
+  ## Create out.dir
+  dir.create(out.dir)
+
+	## Dump catMatrix into out.dir
+	newFileName <- paste0(out.dir,"/",oldFileName,".tsv")
+  write.table(catMatrix, file = newFileName,
+              sep = "\t", quote = F, row.names = F)
+
+
+  invisible(catMatrix)
+}
+
+
--- R/ReadWriteCatalogs.R
+++ R/ReadWriteCatalogs.R
@@ -45,3 +45,28 @@ WriteCatCOMPOSITE <- function(ct, path) {
   write.csv(ct, file = path, row.names = TRUE,
             quote = FALSE)
 }
+
+#' Split COMPOSITE (SNS1536+DBS78+ID83) catalogues
+#' in ICAMS format into 3 individual catalogs.
+#' @param catalog Input catalog, can be a .csv file or matrix
+#' in ICAMS COMPOSITE format.
+#' @return a list, containing 3 catalog matrices in MultiModalMuSig format.
+#' Each matrix contains SNS1536, DBS78 and ID83 information, respectively.
+SplitCatCOMPOSITE <- function(catalog) {
+
+  # Read COMPOSITE catalog. Either from file or matrix-like
+  stopifnot(is.character(catalog) | is.data.frame(catalog) | is.matrix(catalog))
+  if(class(catalog) == "character")
+    catMatrix <- ReadCatCOMPOSITE(catalog)
+  else
+    catMatrix <- catalog
+
+  # Split COMPOSITE catalog to 3 catalogues
+  # (1 SNS1536, 1 DNS78, 1 Indel83)
+  # use function in ReadWriteCatalogs.R
+  catList <- list("SNS1536" = catMatrix[1:1536,],
+                  "DNS78" = catMatrix[1537:1614,],
+                  "ID83" = catMatrix[1615:1697,])
+
+  return(catList)
+}
